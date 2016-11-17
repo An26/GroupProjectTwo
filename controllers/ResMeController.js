974ea49307b106c34f287e732c36ed23f7e589cd
An Huynh <An Huynh>
2016-11-16 20:27:15 -0600

    adding forms

---
+++ controllers/ResMeController.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+//var cat = require('../models/cat.js');
+
+router.get('/', function (req, res) {
+	res.redirect('/index');
+});
+
+module.exports = router;
