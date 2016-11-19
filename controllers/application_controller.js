2040eb8734eb356bd37872fe9efd88701fe7625a
Stella Wu <stellawu@utexas.edu>
2016-11-19 12:58:36 -0600

    adding session variables

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -1,6 +1,8 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
+var fs = require('fs');
+var pdf = require('pdfkit');
 //need to require pdfkit, and global variables for resume defined here
 
 router.get('/', function(req, res) {
@@ -9,16 +11,16 @@ router.get('/', function(req, res) {
 
 router.get('/register', function(req,res) {
 	//add user to database
-	var registerUsername = req.body.registrarUsername;
-	var registerPassword = req.body.registerPassword;
-	models.user.findOne({where:{email:registerUsername}})
+	req.session.registerUsername = req.body.registrarUsername;
+	req.sessopm.registerPassword = req.body.registerPassword;
+	models.user.findOne({where:{email:req.session.registerUsername}})
 	.then(function(existingUser){
 		if(existingUser){
 		//user partial to send error message:'user name already existed, please try another username.'
 		} else {
 			models.user.create({
-				email:registerUsername,
-				password:registerPassword
+				email:req.session.registerUsername,
+				password:req.session.registerPassword
 			})
 		}
 	})
@@ -41,22 +43,22 @@ router.get('/resume/user', function(req, res) {
 
 router.get('/resume/education', function(req,res) {
 	//once the information is validated and next button is hit, render projects
-	var name = req.body.fullName;
-	var street = req.body.street;
-	var rest = req.body.rest;
-	var phoneNumber = req.body.phoneNumber;
-	var githubUrl = req.body.githubUrl;
-	var summary = req.body.summary;
+	var req.session.name = req.body.fullName;
+	var req.session.street = req.body.street;
+	var req.session.rest = req.body.rest;
+	var req.session.phoneNumber = req.body.phoneNumber;
+	var req.session.githubUrl = req.body.githubUrl;
+	var req.session.summary = req.body.summary;
 	//session variable username
-	models.user.findOne({where:{email:registerUsername}})
+	models.user.findOne({where:{email:req.session.registerUsername}})
 	.then(function(currentUser){
 		currentUser.update({
-				name:name,
-				phone:phoneNumber,
-				street:street,
-				rest:rest,
-				github:githubUrl,
-				summary:summary
+				name:req.session.name,
+				phone:req.session.phoneNumber,
+				street:req.session.street,
+				rest:req.session.rest,
+				github:req.session.githubUrl,
+				summary:req.session.summary
 		})
 	})
 	.then(function(updatedUser){
@@ -67,25 +69,25 @@ router.get('/resume/education', function(req,res) {
 
 router.get('/resume/projects', function(req,res) {
 	//once the information is validated and next button is hit, render work
-	var schoolName = req.body.schoolName;
-	var location = req.body.schoolLocation;
-	var major = req.body.major;
-	var degree = req.body.degree;
-	var years = req.body.years;
-	var gpa = req.body.gpa;
-	var honors = req.body.honors;
+	var req.session.schoolName = req.body.schoolName;
+	var req.session.schoolLocation = req.body.schoolLocation;
+	var req.session.major = req.body.major;
+	var req.session.degree = req.body.degree;
+	var req.session.schoolYears = req.body.years;
+	var req.session.gpa = req.body.gpa;
+	var req.session.honors = req.body.honors;
 		//session variable username
 	models.user.findOne({where:{email:registerUsername}})
 	.then(function(currentUser){
 		models.education.create({
 			userId:currentUser.id,
-			schoolName:schoolName,
-			location:location,
-			major:major,
-			degree:degree,
-			years:years,
-			gpa:gpa,
-			honors:honors
+			schoolName:req.session.schoolName,
+			location:req.session.schoolLocation,
+			major:req.session.major,
+			degree:req.session.degree,
+			years:req.session.schoolYears,
+			gpa:req.session.gpa,
+			honors:req.session.honors
 		})
 	})
 	.then(function(education){
@@ -96,18 +98,18 @@ router.get('/resume/projects', function(req,res) {
 
 router.get('/resume/work', function(req,res) {
 	//once the information is validated and next button is hit, render resume preview
-	var projectName = req.body.projectName;
-	var description = req.body.description;
-	var url = req.body.projectUrl;
-	var date = req.body.projectDate;
+	var req.session.projectName = req.body.projectName;
+	var req.session.description = req.body.description;
+	var req.session.url = req.body.projectUrl;
+	var req.session.date = req.body.projectDate;
 	//session variable username
 	models.user.findOne({where:{email:registerUsername}})
 	.then(function(currentUser){
 		models.project.create({
 				userId: currentUser.id,
-				projectName: projectName,
-				url:url,
-				dates:date
+				projectName: req.session.projectName,
+				url:req.session.url,
+				dates:req.session.date
 		})
 	})
 	.then(function(project){
@@ -118,29 +120,30 @@ router.get('/resume/work', function(req,res) {
 
  router.get('/buildResume', function(req,res) {
 // once user hit download, run pdfkit with info above
-	var companyNmae = req.body.companyName;
-	var location = req.body.companyLocation;
-	var title = req.body.title;
-	var years = req.body.years;
-	var responsibility = req.body.responsibility;
-	var duties = req.body.duties;
+	var req.session.companyNmae = req.body.companyName;
+	var req.session.companyLocation = req.body.companyLocation;
+	var req.session.title = req.body.title;
+	var req.session.workYears = req.body.years;
+	var req.session.responsibility = req.body.responsibility;
+	var req.session.duties = req.body.duties;
 	//session variable username
 	models.user.findOne({where:{email:registerUsername}})
 	.then(function(currentUser){
 		models.work.create({
 			userId:currentUser.id,
-			companyName:companyName,
-			location:location,
-			title:title,
-			years:years,
-			responsibilities:responsibility,
-			duties:duties
+			companyName:req.session.companyName,
+			location:req.session.companyLocation,
+			title:req.session.title,
+			years:req.session.workYears,
+			responsibilities:req.session.responsibility,
+			duties:req.session.duties
 		})
 	})
 	.then(function(work){
 		res.render('preview');
 	})
  });
+//generate pdf file using pdfkit
 
 
 
