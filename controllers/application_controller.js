7452b8d769086e537f87d01ff945ec84882f6eaf
Stella Wu <stellawu@utexas.edu>
2016-11-19 11:35:42 -0600

    adding routing comments

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -1,36 +1,44 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
+//need to require pdfkit, and global variables for resume defined here
 
 router.get('/', function(req, res) {
     res.redirect('login');
 });
 
+router.get('/register', function(req,res) {
+	//add user to database
+});
+
 router.get('/login', function(req,res) {
-	res.render('login');
+	//query the database and authenticate the user
+	//if authenticated, render index,
+	//if not, display error message and stay on login
 });
 
 router.get('/resume/user', function(req, res) {
-	res.render('index');
+	//once the information is validated and next button is hit, render education
+
 })
 
 router.get('/resume/education', function(req,res) {
-	res.render('education');
+	//once the information is validated and next button is hit, render projects
 });
 
 
 router.get('/resume/projects', function(req,res) {
-	res.render('projects');
+	//once the information is validated and next button is hit, render work
 });
 
 
 router.get('/resume/work', function(req,res) {
-	res.render('workHistory');
+	//once the information is validated and next button is hit, render resume preview
 });
 
 //can be used later...
 // router.get('/buildResume', function(req,res) {
-// 	res.render('buildResume');
+// once user hit download, run pdfkit with info above
 // });
 
 
