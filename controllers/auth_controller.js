efa15c135357c756a169907a0b7b4360577c9205
Jessica Loch <jessica@belle.local>
2016-11-19 13:30:10 -0600

    changing bcrypt to native js version

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,7 +1,7 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 
 
 router.get('/signin', function(req, res) {
