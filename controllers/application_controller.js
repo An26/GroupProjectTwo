0dbf23e4156a30ce531d618bd600bbd0071a8844
Stella Wu <stellawu@utexas.edu>
2016-11-27 18:01:55 -0600

    adding templates and routes

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -3,7 +3,83 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
+<<<<<<< Updated upstream
     res.render('index');
+=======
+
+	if(req.session.loggedin) {
+		res.render('template');
+	} else {
+    	res.redirect('/signin?e=1');
+	}
+});
+
+router.post('/user/:template',function(req,res){
+	//get the template information
+	req.session.template = req.params.template;
+	console.log('using template '+req.session.template);
+	res.redirect('/resume/user');
+})
+
+router.get('/resume/user', function(req, res) {
+	//once the information is validated and next button is hit, render education
+	res.render('user');
+})
+
+router.post('/education', function(req,res) {
+	//once the information is validated and next button is hit, render projects
+	 req.session.name = req.body.fullName;
+	 req.session.street = req.body.street;
+	 req.session.rest = req.body.rest;
+	 req.session.phoneNumber = req.body.phoneNumber;
+	 req.session.githubUrl = req.body.githubUrl;
+	 req.session.summary = req.body.summary;
+	//session variable username
+	models.user.findOne({where:{email:req.session.registerUsername}})
+	.then(function(currentUser){
+		currentUser.update({
+				name:req.session.name,
+				phone:req.session.phoneNumber,
+				street:req.session.street,
+				rest:req.session.rest,
+				github:req.session.githubUrl,
+				summary:req.session.summary
+		})
+	})
+		res.redirect('/resume/education');
+});
+
+router.get('/resume/education',function(req,res){
+	res.render('education');
+})
+
+router.post('/projects', function(req,res) {
+	//once the information is validated and next button is hit, render work
+	 req.session.schoolName = req.body.schoolName;
+	 req.session.schoolLocation = req.body.schoolLocation;
+	 req.session.major = req.body.major;
+	 req.session.degree = req.body.degree;
+	 req.session.schoolYears = req.body.years;
+	 req.session.gpa = req.body.gpa;
+	 req.session.honors = req.body.honors;
+		//session variable username
+	models.user.findOne({where:{email:req.session.registerUsername}})
+	.then(function(currentUser){
+		models.education.create({
+			userId:currentUser.id,
+			schoolName:req.session.schoolName,
+			location:req.session.schoolLocation,
+			major:req.session.major,
+			degree:req.session.degree,
+			years:req.session.schoolYears,
+			gpa:req.session.gpa,
+			honors:req.session.honors
+		})
+	})
+	.then(function(education){
+		res.redirect('/resume/projects');
+	})
+>>>>>>> Stashed changes
 });
 
 module.exports = router
---
+++ controllers/auth_controller.js
@@ -0,0 +1,92 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+var bcrypt = require('bcrypt-nodejs');
+
+
+router.get('/signin', function(req, res) {
+
+    if(req.query.e) {
+        switch(req.query.e) {
+            case '1':
+                var e = 'Password or Userame is invalid';
+                break;  
+        }
+    }
+    
+    res.render('login', { e: e });
+});
+
+
+router.post('/signin', function(req, res) {
+
+    models.user.findOne({
+        where: { email: req.body.email }
+    }).then(function(user) {
+		if (user == null){
+			res.redirect('/signin?e=1');
+		}
+
+        console.log('User found, checking password');
+
+    // check password
+    bcrypt.compare(req.body.password, user.password, function(err, result) {
+
+        console.log('password complare is ' + result);
+
+      if (result == true){
+        req.session.loggedin = true;
+		req.session.username = user.username;
+        req.session.email = user.email;
+
+        res.redirect('/');
+      }
+      // password is wrong
+      else{
+        res.redirect('/signin');
+	}
+    });
+  });
+});
+
+
+router.get('/signout', function(req, res) {
+    req.session.destroy(function(err) {
+        res.redirect('/')
+  });
+});
+
+
+router.get('/signup', function(req, res) {
+    res.render('signUp');
+});
+
+
+router.post('/signup', function(req, res) {
+
+    // check if user already has an account
+    models.user.findAll({
+        where: { email: req.body.email }
+    }).then(function(users) {
+        if(users.length > 0) {
+            res.send('User already exists!');
+        } else {
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(req.body.password, salt, null, function(err, hash) {
+                    // save to database
+                    models.user.create({
+                        email: req.body.email,
+                        password: hash
+                    }).then( function() {
+                        req.session.loggedin = true;
+                        req.session.username = req.params.username;
+                        res.redirect('/');
+                    });
+                });
+            });
+        }
+    });    
+});
+
+
+module.exports = router;
